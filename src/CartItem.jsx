2f59b35c5b7c9bb3d818eb40356f29097ed7f0f0
miyadang <mdang@smith.edu>
2025-04-06 20:17:20 -0400

    Fix issues adding and removing item in cart

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -31,10 +31,11 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleDecrement = (item) => {
-    dispatch(removeItem(item.name));
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
@@ -7,6 +8,7 @@ function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({}); // State to track added items
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -261,7 +263,7 @@ function ProductList({ onHomeClick }) {
         setAddedToCart((prevState) => ({
            ...prevState,
            [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
-         }));
+        }));
     };
 
 
